d3fd0e9c4a19449c1e317a0a08869922aec5ec5d
YUNJUNG KIM <kyjekffo1105@yahoo.com>
2020-02-05 17:21:44 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,1 +1,38 @@
 // add solution here
+function theBeatlesPlay(musicians, instruments) {
+  var resultArray = [];
+  
+  for(var i = 0; i < musicians.length; i++) {
+    resultArray.push(`${musicians[i]} plays ${instruments[i]}`);
+  }
+  return resultArray;
+}
+
+function johnLennonFacts(facts) {    
+  var resultArray = [];
+  var i = 0;
+  while(facts.length > i) {             
+    resultArray.push(`${facts[i]}!!!`);
+    i++;
+  }
+   return resultArray;
+}
+
+function iLoveTheBeatles(num) {
+  var resultArray = [];
+  
+  if (num < 15) {
+    var i = 0;
+    
+    do {
+     resultArray.push(`I love the Beatles!`);
+     i++;
+    } while(i > (num - 15) );
+    
+  } else {
+    resultArray.push(`I love the Beatles!`);
+  }
+  
+  return resultArray;
+}
+
